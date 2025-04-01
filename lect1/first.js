122cf7f06d41eda42ff174c623ac48a41d408eb7
arsh <arsh40896@gmail.com>
2025-04-01 19:08:25 +0530

    js

---
+++ lect1/first.js
@@ -0,0 +1,24 @@
+// java script is dynamically type language 
+
+let age = 18.2  ; 
+age++; 
+
+console.log(typeof(age))
+x= null; 
+y = undefined;
+// type of variables
+// ( primitive data type ) 1. number 2. string 3. boolean  4. null 5. undefined 5. symbol 6. BigInt
+// ( non primitive data type ) 1. array 2. object ( object store  (key : value)     pair ) 3. function
+
+console.log(typeof(y)) ;
+
+const student = {
+    name : "arsh" ,
+    sap_id : 590013807, 
+    age : 18,
+    sgpa: 9.55,
+}; 
+console.log(student); 
+console.log(typeof(student)); 
+console.log(student.name); 
+
