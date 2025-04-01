944e906b65b9e242c0d73e36822ed49da07045b7
arsh <arsh40896@gmail.com>
2025-04-01 22:11:29 +0530

    some change

---
+++ operator, conditional_statement/second.js
@@ -0,0 +1,43 @@
+// single line comment 
+/*  way to 
+multi line 
+comment */
+number =prompt( "enter number :"); // this is use for taking input 
+console.log(number ) ; 
+
+
+console.log("hello world!")
+console.log("lecture 2")
+let  x = 20 ; // in let we can update its value 
+x = 25; 
+console.log(x) ;
+const y= 30; 
+// we can't update the value of const variable
+// y = 35 // this will give an error
+
+
+let a = 40 ; 
+let b = 10 ;
+console.log(a + b) ;
+console.log(a - b) ;
+console.log(a * b) ;
+console.log(a / b) ;
+console.log(a % b) ;
+// double asterisk means power 
+a = 2 ; 
+b = 3 ;
+console.log(a ** b); 
+let mode= "light";
+let color ; 
+if(mode== "dark"){
+    color = "black" ;
+}
+else if (mode=="light"){
+    color = "sky blue " ;
+}
+else{
+    color = "white" ;
+    }
+console.log(color )
+
+mode =="dark" ? console.log("blue"): console.log("black") ; // single line if else ( conditional statement)
